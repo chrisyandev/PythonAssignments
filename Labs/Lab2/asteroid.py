b46d21bac49e91b1f32b6a67435d4870b60f021b
chrisyandev <czyanhome@hotmail.com>
2021-01-20 15:44:19 -0800

    Added docstrings and comments

--- Labs/Lab2/asteroid.py
+++ Labs/Lab2/asteroid.py
@@ -1,33 +1,62 @@
 class Vector:
+    """This class represents a 3D vector."""
     def __init__(self, vector):
+        """
+        Initializes the state.
+        :param vector: a tuple holding the vector coordinates
+        """
         self.x = vector[0]
         self.y = vector[1]
         self.z = vector[2]
 
     def add(self, vector):
+        """
+        Adds another vector to this one.
+        :param vector: another vector
+        :return: result of adding the vectors
+        """
         self.x += vector.get_x()
         self.y += vector.get_y()
         self.z += vector.get_z()
 
     def get_x(self):
+        """
+        Gets the x coordinate.
+        :return: the x coordinate
+        """
         return self.x
 
     def get_y(self):
+        """
+        Gets the y coordinate.
+        :return: the y coordinate
+        """
         return self.y
 
     def get_z(self):
+        """
+        Gets the z coordinate.
+        :return: the z coordinate
+        """
         return self.z
 
     def get_vector(self):
+        """
+        Gets the vector coordinates as a tuple.
+        :return: a tuple containing the vector coordinates
+        """
         return self.get_x(), self.get_y(), self.get_z()
 
     def __str__(self):
+        """
+        Changes the vector as a tuple into a string.
+        :return: a string representing the vector coordinates
+        """
         return str(self.get_vector())
 
 
 class Asteroid:
     """This class represents an asteroid."""
-
     id_counter = 0
 
     def __init__(self, circumference, position, velocity):
@@ -43,29 +72,55 @@ class Asteroid:
         self._id = self.__generate_id()
 
     def move(self):
+        """
+        Changes the asteroid position based on its velocity.
+        Prints the asteroid's old and new positions.
+        :return: none
+        """
         print('Asteroid {0} moved! Old Pos: {1} '
               .format(self.get_id(), self.get_position()), end="")
         self._position.add(self._velocity)
         print('-> New Pos: {0}'.format(self.get_position()))
 
     def get_circumference(self):
+        """
+        Gets the asteroid's circumference.
+        :return: the asteroid's circumference
+        """
         return self._circumference
 
     def get_position(self):
+        """
+        Gets the asteroid's position.
+        :return: the asteroid's position
+        """
         return self._position
 
     def get_velocity(self):
+        """
+        Gets the asteroid's velocity.
+        :return: the asteroid's velocity
+        """
         return self._velocity
 
     def get_id(self):
+        """
+        Gets the asteroid's unique id.
+        :return: the asteroid's unique id
+        """
         return self._id
 
     @classmethod
     def __generate_id(cls):
+        # Returns a new id
         cls.id_counter += 1
         return cls.id_counter
 
     def __str__(self):
+        """
+        Returns a formatted string containing information about the asteroid.
+        :return: a formatted string
+        """
         return 'Asteroid: {0} is currently at {2} and moving at {3} ' \
                'meters per second. It has a circumference of {1}.'\
             .format(self.get_id(), self.get_circumference(),
--- Labs/Lab2/controller.py
+++ Labs/Lab2/controller.py
@@ -6,10 +6,19 @@ from asteroid import *
 
 
 class Controller:
-
+    """This class controls the simulation of asteroids in space."""
     asteroids = []
 
     def __init__(self, num_of_asteroids, min_radius, max_radius, cube_length, max_speed):
+        """
+        Creates Asteroid instances with randomized properties
+        limited by this method's arguments.
+        :param num_of_asteroids: the number of asteroids to create
+        :param min_radius: an asteroid's minimum possible radius
+        :param max_radius: an asteroid's maximum possible radius
+        :param cube_length: the length of the space containing the asteroids
+        :param max_speed: max value for an asteroid's velocity vector
+        """
         for x in range(num_of_asteroids):
             new_asteroid = Asteroid(self.__random_circumference(min_radius, max_radius),
                                     self.__random_vector(-(cube_length/2), cube_length/2),
@@ -18,10 +27,17 @@ class Controller:
 
     @classmethod
     def simulate(cls, seconds):
+        """
+        Moves all asteroids once every second, on the second.
+        If the time is partway through a second, execution is paused.
+        :param seconds: the duration of the simulation
+        :return: none
+        """
         target_time = datetime.datetime.now().second + 1
         while datetime.datetime.now().second < target_time:
             pass
         print('Simulation Start Time: ', datetime.datetime.now())
+        print('\nMoving Asteroids!\n-----------------')
         target_time += 1
         for s in range(seconds):
             for a in cls.asteroids:
@@ -33,6 +49,7 @@ class Controller:
 
     @staticmethod
     def __random_vector(min, max):
+        # Returns a Vector with random coordinates
         x = random.randint(min, max)
         y = random.randint(min, max)
         z = random.randint(min, max)
@@ -40,11 +57,16 @@ class Controller:
 
     @staticmethod
     def __random_circumference(min_radius, max_radius):
+        # Calculates the circumference based on a random radius
         radius = random.randint(min_radius, max_radius)
         return 2 * math.pi * radius
 
 
 def main():
+    """
+    Drives the program.
+    :return: none
+    """
     controller = Controller(100, 1, 4, 100, 5)
     controller.simulate(5)
     print('finished simulation')
